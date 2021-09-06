b0c019988bf6d95db42eb56f78b438e930dc1142
anmrez <zzirg1@mail.ru>
2021-09-06 09:08:16 +0300

    MoGo app

--- site/MoGo/js/app.js
+++ site/MoGo/js/app.js
@@ -48,3 +48,53 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
         });
     });
 });
+
+
+// navigation scrollBy
+
+const scrollElement = [ document.querySelector(".blok__1"),
+                      document.querySelector(".blok__5"),
+                      document.querySelector(".bloc__9"),
+                      document.querySelector(".block__12"),
+                      document.querySelector(".block__15")];
+const buddle = document.querySelector(".buddle");
+const option = {
+  threshold: 0.1
+
+};
+
+
+let obsever = new IntersectionObserver(navCheck, option);
+
+
+
+
+function navCheck(entries){
+  entries.forEach(entry =>{
+    const className = entry.target.className;
+    // console.log(className);
+    const activeAnchor = document.querySelector(`[data-page=${className}]`)
+    // console.log(activeAnchor);
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+        height: coords.height,
+        width: coords.width,
+        left: coords.left,
+        top: coords.top
+    };
+    if (entry.isIntersecting){
+      buddle.style.setProperty('left', `${directions.left}px`);
+      buddle.style.setProperty('top', `${directions.top}px`);
+      buddle.style.setProperty('width', `${directions.width}px`);
+      buddle.style.setProperty('height', `${directions.height}px`);
+    };
+
+  });
+
+}
+
+
+  scrollElement.forEach(section => {
+    obsever.observe(section);
+    // console.log(section);
+  })
